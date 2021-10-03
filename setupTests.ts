b7aacb91054d7fcdf1808b06a83c11d719024205
Atharva Bhange <atharva.bhange@gmail.com>
2021-10-04 02:00:05 +0530

    Initial Setup Complete

---
+++ setupTests.ts
@@ -0,0 +1,7 @@
+beforeEach(async () => {
+	console.log("Before Each");
+});
+
+afterEach(async () => {
+	console.log("After Each");
+});
---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { config } from "dotenv";
+config();
+import express, { json } from "express";
+import morgan from "morgan";
+
+const app = express();
+
+const { LATENCY = "0" } = process.env;
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use(json());
+app.use((_, __, next) => {
+	setTimeout(() => {
+		next();
+	}, parseInt(LATENCY));
+});
+
+app.get("/", (req, res) => {
+	res.status(200).json({
+		status: "success",
+	});
+});
+
+export default app;
